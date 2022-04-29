ad5ce4727aef265ab8c7f3d61c0f2c7b2a20df42
kartick sriram <sriram5130@gmail.com>
2022-04-29 22:50:41 +0530

    implementing blueprint - seperated login

--- app.py
+++ app.py
@@ -1,36 +1,21 @@
 from flask import Flask, jsonify, request, render_template, redirect, url_for, session
 from flask_restful import Resource, Api
 import sqlite3, uuid
-from login import home_page
+from login import login_page, check_page
 
 app = Flask(__name__, template_folder="templates")
 #api = Api(app)
 app.config["SECRET_KEY"] = "hello"
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-app.register_blueprint(home_page)
+app.register_blueprint(login_page)
+app.register_blueprint(check_page)
 
-# ~ @app.route("/login")
-# ~ def login():
-	# ~ return render_template("login.html")
-
-# ~ @app.route("/login_check", methods=["POST"])
-# ~ def login_check():
-	# ~ result = request.form.to_dict()
-	# ~ login_data = (result["username"], result["password"])
-	# ~ lstatus = check_login(login_data)
-	# ~ if lstatus:
-		# ~ session["uid"] = result["username"]
-	# ~ return redirect(url_for("index"))
-
-# ~ def check_login(ldata):
-	# ~ conn = sqlite3.connect("./database/bookstore.db")
-	# ~ cursor = conn.cursor()
-	# ~ cursor.execute("select count(*) from login where name=? and password=?",ldata)
-	# ~ if(cursor.fetchone()[0]):
-		# ~ conn.close()
-		# ~ return True
-	# ~ conn.close()
+@app.route("/")
+def index():
+	if not session.get("uid"):
+		return redirect("/login")
+	return render_template("index.html")
 
 # ~ @app.route("/logout")
 # ~ def logout():
--- login.py
+++ login.py
@@ -1,10 +1,28 @@
 from flask import Flask, jsonify, request, render_template, redirect, url_for, session, Blueprint
+import sqlite3
 
-home_page=Blueprint("home_page", __name__, template_folder="templates")
+login_page=Blueprint("login_page", __name__, template_folder="templates")
 
+@login_page.route("/login")
+def login():
+	return render_template("login.html")
+	
+check_page=Blueprint("check_page", __name__, template_folder="templates")
 
-@home_page.route("/")
-def index():
-	# ~ if not session.get("uid"):
-		# ~ return redirect(url_for("login"))
-	return render_template("index.html")
+@check_page.route("/login_check", methods=["POST"])
+def login_check():
+	result = request.form.to_dict()
+	login_data = (result["username"], result["password"])
+	lstatus = check_login(login_data)
+	if lstatus:
+		session["uid"] = result["username"]
+	return redirect(url_for("index"))
+
+def check_login(ldata):
+	conn = sqlite3.connect("./database/bookstore.db")
+	cursor = conn.cursor()
+	cursor.execute("select count(*) from login where name=? and password=?",ldata)
+	if(cursor.fetchone()[0]):
+		conn.close()
+		return True
+	conn.close()

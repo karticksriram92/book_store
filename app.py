9a75b2a15c5f49a8a9d14b6f311d113a14cf2f14
kartick sriram <sriram5130@gmail.com>
2022-05-01 20:13:49 +0530

    trying WTforms

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, render_template, redirect, url_for, session
 from flask_restful import Resource, Api
 import sqlite3, uuid
-from login import login_page, check_page
+from login import login_page, check_page, register_page
 
 app = Flask(__name__, template_folder="templates")
 #api = Api(app)
@@ -10,6 +10,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 app.register_blueprint(login_page)
 app.register_blueprint(check_page)
+app.register_blueprint(register_page)
 
 @app.route("/")
 def index():
@@ -17,10 +18,10 @@ def index():
 		return redirect("/login")
 	return render_template("index.html")
 
-# ~ @app.route("/logout")
-# ~ def logout():
-	# ~ session.pop("uid", None)
-	# ~ return redirect(url_for("index"))
+@app.route("/logout")
+def logout():
+	session.pop("uid", None)
+	return redirect(url_for("index"))
 
 # ~ class Test(Resource):
 	# ~ def get(self):
--- login.py
+++ login.py
@@ -26,3 +26,29 @@ def check_login(ldata):
 		conn.close()
 		return True
 	conn.close()
+
+register_page=Blueprint("register_page", __name__, template_folder="templates")
+
+@register_page.route("/register")
+def register():
+	return render_template("register.html")
+	
+register_check = Blueprint("register_check", __name__, template_folder="templates")
+	
+@register_check.route("/register_check")
+def check_register():
+	result = request.form.to_dict()
+	login_data = (result["username"], result["password"], result["address"])
+	rstatus = do_register(register_data)
+	if rstatus:
+		session["uid"] = result["username"]
+	return redirect(url_for("index"))
+	
+def do_register(rdata):
+	conn = sqlite3.connect("./database/bookstore.db")
+	cursor = conn.cursor()
+	cursor.execute("select count(*) from login where name=? and password=?",ldata)
+	if(cursor.fetchone()[0]):
+		conn.close()
+		return True
+	conn.close()

2b4a112a93e6ed8a3cc828cfc1cc021db80470b1
kartick sriram <sriram5130@gmail.com>
2022-05-01 13:44:13 +0530

    implementing blueprint- changing login link

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, render_template, redirect, url_for, session
 from flask_restful import Resource, Api
 import sqlite3, uuid
-from login import login_page, check_page
+from login import login_page, check_page, register_page
 
 app = Flask(__name__, template_folder="templates")
 #api = Api(app)
@@ -9,13 +9,33 @@ app.config["SECRET_KEY"] = "hello"
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 app.register_blueprint(login_page)
-app.register_blueprint(check_page)
+# ~ app.register_blueprint(check_page)
+app.register_blueprint(register_page)
 
-@app.route("/")
+@app.route("/", methods=["POST","GET"])
 def index():
 	if not session.get("uid"):
-		return redirect("/login")
+		return render_template("login.html")
+	if request.method=="POST":
+		login_check(request)
 	return render_template("index.html")
+	
+def login_check(request):
+	result = request.form.to_dict()
+	login_data = (result["username"], result["password"])
+	lstatus = check_login(login_data)
+	if lstatus:
+		session["uid"] = result["username"]
+	return redirect(url_for("index"))
+
+def check_login(ldata):
+	conn = sqlite3.connect("./database/bookstore.db")
+	cursor = conn.cursor()
+	cursor.execute("select count(*) from login where name=? and password=?",ldata)
+	if(cursor.fetchone()[0]):
+		conn.close()
+		return True
+	conn.close()
 
 # ~ @app.route("/logout")
 # ~ def logout():
--- login.py
+++ login.py
@@ -7,22 +7,31 @@ login_page=Blueprint("login_page", __name__, template_folder="templates")
 def login():
 	return render_template("login.html")
 	
-check_page=Blueprint("check_page", __name__, template_folder="templates")
+# ~ check_page=Blueprint("check_page", __name__, template_folder="templates")
 
-@check_page.route("/login_check", methods=["POST"])
-def login_check():
-	result = request.form.to_dict()
-	login_data = (result["username"], result["password"])
-	lstatus = check_login(login_data)
-	if lstatus:
-		session["uid"] = result["username"]
-	return redirect(url_for("index"))
+# ~ @check_page.route("/login_check", methods=["POST"])
+# ~ def login_check():
+	# ~ result = request.form.to_dict()
+	# ~ login_data = (result["username"], result["password"])
+	# ~ lstatus = check_login(login_data)
+	# ~ if lstatus:
+		# ~ session["uid"] = result["username"]
+	# ~ return redirect(url_for("index"))
 
-def check_login(ldata):
-	conn = sqlite3.connect("./database/bookstore.db")
-	cursor = conn.cursor()
-	cursor.execute("select count(*) from login where name=? and password=?",ldata)
-	if(cursor.fetchone()[0]):
-		conn.close()
-		return True
-	conn.close()
+# ~ def check_login(ldata):
+	# ~ conn = sqlite3.connect("./database/bookstore.db")
+	# ~ cursor = conn.cursor()
+	# ~ cursor.execute("select count(*) from login where name=? and password=?",ldata)
+	# ~ if(cursor.fetchone()[0]):
+		# ~ conn.close()
+		# ~ return True
+	# ~ conn.close()
+	
+
+register_page=Blueprint("register_page", __name__, template_folder="templates")
+
+@register_page.route("/register")
+def register():
+	return render_template("register.html")
+	
+
